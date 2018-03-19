36b121811d21b31a03e25a605b4408ad15a3429e
adam n <adam.nawroc@gmail.com>
2018-03-19 16:29:12 +0000

    automatically backed up by learn

--- db/migrate/20180319162321_create_movies.rb
+++ db/migrate/20180319162321_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
       t.string :title
-      t.release_date :genre
+      t.integer :release_date
       t.integer :age
       t.string :hometown
     end
