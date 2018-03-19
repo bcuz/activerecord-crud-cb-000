c39aa444bf93a984ca933f185671a93246428961
adam n <adam.nawroc@gmail.com>
2018-03-19 16:28:39 +0000

    automatically backed up by learn

--- db/migrate/20180319162321_create_movies.rb
+++ db/migrate/20180319162321_create_movies.rb
@@ -1,7 +1,7 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :students do |t|
-      t.string :name
+      t.string :title
       # t.string :genre
       # t.integer :age
       # t.string :hometown
