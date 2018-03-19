ee80eb89fb5e3b9f44b8262bcdd93f8dfc1a47b0
adam n <adam.nawroc@gmail.com>
2018-03-19 17:08:30 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -38,7 +38,8 @@ def can_be_created_in_a_block(args = {title: 'Home Alone', release_date: 1990})
     # binding.pry
 
     # how can a movie receive a title w/o receving anything?
-    m.title =
+    m.title = args['title']
+    m.release_date = args['release_date']
 
     # m is a movie
   end
